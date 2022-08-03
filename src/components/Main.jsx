c24aa72c0a42085a1f9f62ea3b037b3a1cf2126e
Mohamed Ibrahim Fathy <eng.mohammedibrahim904@gmail.com>
2022-08-03 12:19:16 +0200

    Pokemon App Done!

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,9 @@ import List from './List'
 export default function Main() {
   return (
     <div className="main-container">
-        <nav>Catch 'em All:Pokemons</nav>
+        <nav>
+            <img src={require('../assets/header-logo.png')} alt="Header Logo" />
+        </nav>
         <h1>All Pokemons</h1>
         <List></List>
     </div>
